9410ad78853d643afcdb08576e1a268bded3702f
maxime vincent <maxime.vincent1@u-psud.fr>
2023-08-19 23:16:28 +0200

    Création d'une nouvelle partie

--- browser.js
+++ browser.js
@@ -1,6 +1,7 @@
 const ws = new WebSocket("ws://localhost:8080");
 
 let pseudo = "";
+let game = "";
 
 const send = (json) => ws.send(JSON.stringify(json));
 
@@ -14,25 +15,62 @@ ws.onclose = function(ev) {
     // TODO: Revenir à la page d'accueil
 };
 
+// Réception des messages du serveur
 ws.onmessage = function(ev) {
     const data = JSON.parse(ev.data);
     console.log(data);
 
-    if (data["register"] === pseudo) {
-        const p = document.createElement("p");
-        p.innerHTML = data["accepted"] ? "Connexion réussie !" : "Échec de connexion.";
-        document.querySelector("body").appendChild(p);
+    // Enregistrer le joueur
+    if (data["register"]) {
+        if (data["register"] === pseudo && data["accepted"]) {
+            // Affichage du pseudo
+            document.getElementById("pseudo").innerHTML = pseudo;
+
+            // Formulaire de création de partie
+            const main = document.querySelector("main")
+            main.innerHTML = "";
+
+            const game_input = document.createElement("input")
+            game_input.id = "game_input";
+            game_input.setAttribute("type", "text");
+            main.appendChild(game_input);
+
+            const create_button = document.createElement("button");
+            create_button.innerHTML = "Créer une partie";
+            create_button.onclick = create_game;
+            main.appendChild(create_button);
+
+        } else {
+            document.getElementById("pseudo_input").style = "border-color: red";
+        }
+    }
+
+    // Créer une partie
+    else if (data["create_game"]) {
+        if (data["create_game"] === game && data["accepted"]) {
+            document.getElementById("game_input").style = "border-color: green";
+            // Affichage du nom de partie
+            document.getElementById("game").innerHTML = game;
+
+            // TODO: Formulaire de choix du mot secret
+
+        } else {
+            document.getElementById("game_input").style = "border-color: red";
+        }
     }
 };
 
 function register() {
-    pseudo = document.getElementById("pseudo").value;
-    if (pseudo !== "") {
-        send({"register": pseudo});
-    }
+    pseudo = document.getElementById("pseudo_input").value;
+    if (pseudo) { send({"register": pseudo}); }
 }
 
 function unregister() {
     send({"unregister": pseudo});
     pseudo = "";
+}
+
+function create_game() {
+    game = document.getElementById("game_input").value;
+    if (game) { send({"create_game": game}) };
 }
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({port: 8080});
 
-const players = new Set();
+const players = {};
+const games = {};
 
 wss.on("connection", function(ws) {
     let pseudo = ""; // Pseudo du joueur courant
@@ -15,17 +16,30 @@ wss.on("connection", function(ws) {
 
         // Enregistrer le joueur
         if (data["register"]) {
-            const isvalid = (pseudo === "" && !players.has(data["register"])) || pseudo === data["register"];
+            const isvalid = (pseudo === "" && !players.hasOwnProperty(data["register"])) || pseudo === data["register"];
             if (isvalid) {
                 pseudo = data["register"];
                 console.log("<", pseudo, "is registered >");
-                players.add(pseudo); 
+                players[pseudo] = {"ws": ws, "game": "", "role": "", "ndef": 0, "def": {}}; 
             }
             send({"register": data["register"], "accepted": isvalid});
         }
         
         // Déconnecter le joueur
         else if (data.hasOwnProperty("unregister")) { ws.close(); }
+
+        // Créer une partie
+        else if (data["create_game"]) {
+            const game = data["create_game"];
+            const isvalid = !games.hasOwnProperty(game) && players[pseudo]["game"] === "";
+            if (isvalid) {
+                console.log("< game", game, "is created >");
+                players[pseudo]["role"] = "leader";
+                players[pseudo]["game"] = game;
+                games[game] = {"secret": "", "letters": 1, "words": new Set(), "players": new Set([pseudo])};
+            }
+            send({"create_game": game, "accepted": isvalid});
+        }
         
         // Message incorrect
         else { send({"accepted": false}); }
@@ -34,8 +48,11 @@ wss.on("connection", function(ws) {
     // Déconnexion du joueur
     ws.onclose = function() { 
         console.log("<", pseudo, "is unregistered >");
+        // TODO: Avertir les autres joueurs si partie en cours
+        // Si le joueur est leader la partie s'arrête
+
         // Libérer la mémoire
-        players.delete(pseudo);
+        delete players[pseudo];
     }
 
     //ws.on('error', console.error);
